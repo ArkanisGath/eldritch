2c0e807efc0bcde10a45b9b2e53551dd043d635b
ArkanisGath <hyora45xrd@gmail.com>
2018-09-08 21:16:36 +0200

    wip player movement and container
    player moves on clic using a navmesh

---
+++ Assets/scripts/container_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class container_script : MonoBehaviour
+{
+    [SerializeField]
+    bool locked;
+    [SerializeField]
+    public Transform position_to_open;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public Vector3 reach_container(Transform position_to_open)
+    {
+        return position_to_open.position;
+    }
+
+    public void open_container()
+    {
+        locked = false;
+    }
+}
---
+++ Assets/scripts/playercontrols.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playercontrols : MonoBehaviour
+{
+
+    Vector3 waypoint;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "ground") waypoint = hit.point;
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "container")
+            {
+                waypoint = hit.collider.gameObject.GetComponent<container_script>().reach_container(hit.collider.gameObject.GetComponent<container_script>().position_to_open);
+            }
+            GetComponent<NavMeshAgent>().destination = waypoint;
+        }
+    }
+
+
+
+}
