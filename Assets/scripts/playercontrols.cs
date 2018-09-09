e7fc9e19712f698ca526389a15b5ad3fabd59269
ArkanisGath <hyora45xrd@gmail.com>
2018-09-09 10:26:59 +0200

    simple animator for biped character
    animator for walk and idle added
    script on player controls changed to implement those behaviors

--- Assets/scripts/playercontrols.cs
+++ Assets/scripts/playercontrols.cs
@@ -7,27 +7,41 @@ public class playercontrols : MonoBehaviour
 {
 
     Vector3 waypoint;
+    [SerializeField]
+    string state;
     // Use this for initialization
     void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        AnimationHandler();
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "ground") waypoint = hit.point;
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "ground")
+            {
+                state = "walking";
+                waypoint = hit.point;
+            }
             if (Physics.Raycast(ray, out hit) && hit.collider.tag == "container")
             {
                 waypoint = hit.collider.gameObject.GetComponent<container_script>().reach_container(hit.collider.gameObject.GetComponent<container_script>().position_to_open);
             }
             GetComponent<NavMeshAgent>().destination = waypoint;
         }
+
+        
     }
 
 
-
+    void AnimationHandler()
+    {
+        if (state == "walking") GetComponent<Animator>().SetInteger("state", 1);
+        if (GetComponent<NavMeshAgent>().remainingDistance <= 0.01f) state = "idle";
+        if (state=="idle") GetComponent<Animator>().SetInteger("state", 0);
+    }
 }
